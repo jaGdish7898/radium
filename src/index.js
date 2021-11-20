ebc5e00e4edaaa439a333de756e217abfd813bf3
jaGdish7898 <93571870+jaGdish7898@users.noreply.github.com>
2021-11-20 16:02:15 +0530

    middleWare Assignmrnt

--- src/index.js
+++ src/index.js
@@ -8,6 +8,35 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// witing global midware:
+
+ let mid=function(req,res,next){
+    let all=new Date();  //2021-11-20T08:20:03.330Z
+    let date=all.getDate()
+    let month=all.getMonth()+1;
+    let year=all.getFullYear();
+    let hour=all.getHours();
+    let minutes=all.getMinutes();
+    let seconds=all.getSeconds();
+    
+//LIKE THIS WE CAN SEPRATE THE OUTPUT OF NEW DATE
+    let fullDate=date+"|"+month+"|"+year;
+    let fullTime=hour+"|"+minutes+"|"+seconds;
+    let ipOfApi=req.ip;
+    let url=req.originalUrl;
+    console.log(fullTime,ipOfApi,url)
+    console.log(fullDate)
+    // console.log(all.toLocaleDateString())
+     next();
+ }
+ app.use(mid)
+
+
+
+
+
+
+
 const mongoose = require('mongoose')
 
 mongoose.connect("mongodb+srv://user-open-to-all:hiPassword123@cluster0.xgk0k.mongodb.net/jagdishshinde-database?retryWrites=true&w=majority", {useNewUrlParser: true})
--- src/routes/route.js
+++ src/routes/route.js
@@ -10,7 +10,7 @@ router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
- //api's about books
+ //api's about books//
 router.post('/createAuthor', authorController.createAuthor  );
 
 router.get('/getAllBooks',  bookCantroller.getAllBook  );
@@ -18,7 +18,7 @@ router.get('/getAllBooks',  bookCantroller.getAllBook  );
 //api's about author
 router.post("/addThisBook",bookCantroller.addThisBook);
 
-//api's about publisher
+//api's about publisher r
 router.post("/addPublisher",publisherController.addPublisher)
 
 
