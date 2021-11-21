d284d44a393c3989398bd42ebd97db3726ec15b7
jaGdish7898 <93571870+jaGdish7898@users.noreply.github.com>
2021-11-21 10:33:58 +0530

    this is advance ref populate

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -13,11 +13,12 @@ const getUsersData= async function (req, res) {
 } 
 
 const justChill= async function (req, res) {
-    
-
+ //1)populating only favAuthor
 // let output=await UserModel.find().populate("favAuthor");
+//2) populating favAuthor and publisherInfo
 let ans=await UserModel.find().populate("favAuthor").populate("publisherInfo").sort({firstName:1})
-// let ans=await UserModel.find().sort({firstName:1})
+// 3)how to use select with the populated info:
+// let ans=await UserModel.find().populate("favAuthor",{firstName:1,lastName:1,_id:0}).populate("publisherInfo",{firstName:1,age:1,_id:0})
 res.send(ans) 
 }
 module.exports.createUser= createUser
