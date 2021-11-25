4fa44acee29735846b44639c5c1294a071830543
jaGdish7898 <93571870+jaGdish7898@users.noreply.github.com>
2021-11-25 15:20:38 +0530

    axiom

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,22 +1,14 @@
 const express = require('express');
 const router = express.Router();
-
-const UserController= require("../controllers/userController")
-const middleWare=require("../middleWares/localMiddleWare")
-
-
-
-
+const cowinController= require("../controllers/cowinController")
 
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
-
-// Creating User
-router.post('/createUser',UserController.createUser);
-router.post('/login',UserController.login);
-router.get('/user/:userId',middleWare.tokenCheaker,UserController.getUser)
-router.put('/user/:userId',middleWare.tokenCheaker,UserController.update)
-router.post("/getThis",UserController.getThis)
+router.get("/cowin/states", cowinController.getStatesList)
+router.get("/cowin/districts/:stateId", cowinController.getDistrictsList)
+router.get("/cowin/centers", cowinController.getByPin)
+router.post("/cowin/getOtp", cowinController.getOtp)
+router.post("/confirmOtp", cowinController.confirmOtp)
 module.exports = router;
